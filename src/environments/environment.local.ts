7299d70faf7b48e30dd2bbb57e463e776d6eeefe
Chris Yang <chris.yang@cupix.com>
2022-12-20 14:04:40 +0900

    test environment

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -6,15 +6,15 @@
 export const environment = {
   production: false,
   authorization: {
-    clientId: "",
+    clientId: "spa-f0Hb4QF3pQMrmy7iCOs0YQWh1",
     scope: "imodelaccess:read imodels:read realitydata:read",
-    redirectUri: "http://localhost:3000",
-    postSignoutRedirectUri: "http://localhost:3000",
+    redirectUri: "http://localhost:3000/auth",
+    // postSignoutRedirectUri: "http://localhost:3000",
     responseType: "code",
     authority: "https://ims.bentley.com"
   },
-  iTwinId: "",
-  iModelId: "",
+  iTwinId: "7465e5c1-5c6a-4b7c-ac29-fc226f4bd66b",
+  iModelId: "0f5e1e73-60c0-43b3-90a6-cc4e4cde7e03",
   map: {
     bingKey: ""
   }
